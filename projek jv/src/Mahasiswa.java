6a4f0047a2ff8810e4f0330a30d219b0962040ba
Satria <satria.salam2005@gmail.com>
2025-07-17 16:36:39 +0700

    Menghapus beberapa isi dalam file Mahasiswa.java

--- projek jv/src/Mahasiswa.java
+++ projek jv/src/Mahasiswa.java
@@ -1,7 +1,5 @@
 public class Mahasiswa {
     public static void main(String[] args) throws Exception {
-        System.out.println("Satria Badrus");
-        System.out.println("Novika Salsa");
         System.out.println("Bagus");
         System.out.println("Rio");
         System.out.println("Santoso");
