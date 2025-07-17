f78336a92ad80fbfb570157e031fad6c460d709f
Satria <satria.salam2005@gmail.com>
2025-07-17 14:09:59 +0700

    Memodifikasi file

--- projek jv/src/Mahasiswa.java
+++ projek jv/src/Mahasiswa.java
@@ -2,6 +2,6 @@ public class Mahasiswa {
     public static void main(String[] args) throws Exception {
         System.out.println("Satria Badrus");
         System.out.println("Novika Salsa");
-        
+        System.out.println("Rani");
     }
 }
